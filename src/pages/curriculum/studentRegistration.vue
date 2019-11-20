7dc2b7c7246e54d2a49825c79256fa0c42c9158c
浦春杰 <puchunjie@live.com>
2019-11-20 17:59:18 +0800

    教师-课程详情 教师-学员签到

---
+++ src/pages/curriculum/studentRegistration.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <zxTabs :options="tabs" large></zxTabs>
+    </div>
+</template>
+
+<script>
+import zxTabs from '@/components/tabs.vue'
+export default {
+    components: { zxTabs },
+    data() {
+        return {
+            tabs: [{
+                    label: '批量签到',
+                    value: 1
+                },
+                {
+                    label: '扫码签到',
+                    value: 2
+                }
+            ]
+        }
+    },
+}
+</script>
+
+<style lang="less">
+
+</style>
