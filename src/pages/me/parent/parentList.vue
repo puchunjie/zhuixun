c307b461bb576dd8924e40ae7b54a368a5d6a51e
jswxgx <287386860@qq.com>
2019-11-27 17:52:42 +0800

    家长列表
    Signed-off-by: jswxgx <287386860@qq.com>

---
+++ src/pages/me/parent/parentList.vue
@@ -0,0 +1,120 @@
+<template>
+    <div class="parent-mana-container">
+		<div class="top">
+			<p>全部家长：{{ parentList.length }}</p>
+		</div>
+        <div class="item" v-for="item in parentList" :key="item.parentId">
+            <image class="gender-img" :src="'/static/head2.png'"></image>
+            <div class="info">
+                <h3 class="name">{{ item.realName}}</h3>
+				<p class="phone">{{ item.mobilePhone}}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+    data() {
+        return {
+            parentList: [],
+        }
+    },
+	computed: {
+	    ...mapGetters(['userinfo'])
+	},
+	onLoad() {
+		this.getParentList();
+	},
+	methods: {
+		getParentList() {
+			uni.request({
+			    method: 'POST',
+			    url: `${this.doMain}/parent/list`,
+			    header: {
+			        'content-type': 'application/x-www-form-urlencoded'
+			    },
+			    data: { shopId: this.userinfo.shopId,pageNo:0,pageSize:1000},
+			    success: res => {
+			        if (res.data.code === 0) {
+			            this.parentList = res.data.data;
+			        }else{
+						uni.showToast({
+							title:res.data.fieldErrors[0].message,
+							icon: 'none',
+							duration: 1000
+						})
+					}
+			    }
+			});
+		}
+	},
+}
+</script>
+
+<style lang="less">
+@import url('../../../styles/base.less');
+page {
+	background:#F2F2F2;
+	padding: 0 40upx;
+}
+
+.parent-mana-container {
+    padding-top: 18upx;
+	border-radius: 8upx;
+	.top {
+		background:#FFFFFF;
+		border-bottom: 20upx;
+		width: 100%;
+	}
+    .item {
+		background:#FFFFFF;
+        position: relative;
+        display: flex;
+        width: 100%;
+        padding: 20upx 30upx 30upx;
+		border: 1px solid rgba(230, 230, 230, 1);
+        overflow: hidden;
+        .gender-img {
+            display: block;
+            width: 106upx;
+            height: 106upx;
+            border-radius: 106upx;
+            border: 5upx solid rgba(236, 246, 244, 1);
+        }
+        .info {
+            flex: 1;
+            margin-left: 25upx;
+            .phone {
+                font-size: 30upx;
+                color: #181818;
+                line-height: 30upx;
+            }
+            .name {
+                font-size: 28upx;
+                color: @base_gray;
+                margin-top: 20upx;
+            }
+        }
+        .ac-btn {
+            position: absolute;
+            width: 150upx;
+            height: 44upx;
+            line-height: 44upx;
+            text-align: center;
+            font-size: 24upx;
+            color: #fff;
+            background: rgba(118, 189, 212, 1);
+            border-radius: 8upx 0 8upx 8upx;
+            right: 0;
+            top: 0;
+        }
+        &.active {
+            .ac-btn {
+                background: @base_green;
+            }
+        }
+    }
+}
+</style>
