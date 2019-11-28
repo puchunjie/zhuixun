b1cc9d238e37096385d966938561a460f797c1e2
浦春杰 <puchunjie@live.com>
2019-11-28 10:29:58 +0800

    增加md5

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App'
 import store from '@/store'
 import * as filters from '@/utils/filter.js'
 import { ScEnumKeys, ScEnums } from '@/utils/enums'
+import md5 from 'js-md5'
 import './styles/iconfont.css';
 
 Vue.config.productionTip = false
@@ -12,6 +13,7 @@ Vue.prototype.$clearData = obj => JSON.parse(JSON.stringify(obj));
 Vue.prototype.ScEnumKeys = ScEnumKeys;
 Vue.prototype.ScEnums = ScEnums;
 Vue.prototype.$store = store;
+Vue.prototype.$md5 = md5;
 
 Vue.prototype.$setTitle = title => {
   wx.setNavigationBarTitle({ title });
