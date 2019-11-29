70e6ce59bbfe2cc4c3e74d01b81065490d16feae
jswxgx <287386860@qq.com>
2019-11-29 17:41:18 +0800

    家长-上课历史完成

--- src/pages/me/course/learnedLessonList.vue
+++ src/pages/me/course/learnedLessonList.vue
@@ -1,19 +1,31 @@
 <template>
-    <div class="clsaa-module">
-		<div class="item">
-			<div class="top">
-				<p>班级总数：{{ courseClassList.length }}</p>
-			</div>
-		</div>
-        <div class="item"  v-for="item in courseClassList" :key="item.classId">
+    <div class="lesson-module">
+        <div class="item"  v-for="item in lessonList" :key="item.studentLessonId">
             <div class="top">
-                <p>班级：{{ item.className }}</p>
+				<div class="tit">
+					课程：{{item.courseName}}
+					<div v-show="item.lessonType == 1" class="state-ttg">试听课</div>
+				</div>
+				<div class="detail">
+					<div class="title">消耗课时：</div>
+					<div class="content">{{item.classHours == null ? 0 :item.classHours}}节</div>
+				</div>
+				<div class="detail">
+					<div class="title">学生：</div>
+					<div class="content">{{item.studentName}}</div>
+				</div>
+				<div class="detail">
+					<div class="title">时间：</div>
+					<div class="content">{{item.startTime | dateformatYMD}}  {{item.startTime | dateformatHM}}-{{item.endTime | dateformatHM}}</div>
+				</div>
             </div>
-            <div class="person-nums">
-                <p>老师：{{ item.teacherName }}</p>
-            </div>
+            <div class="bottom">
+				签到时间：
+				<span  v-show="item.state !=3">{{item.signTime | dateformat}} </span>
+				<span  v-show="item.state ==3">旷课未签到 </span>
+			</div>
         </div>
-        <p v-if="courseClassList.length === 0" class="no-class">暂无班级~</p>
+        <p v-if="lessonList.length === 0" class="no-lesson">暂无记录~</p>
     </div>
 </template>
 
@@ -22,27 +34,27 @@ import { mapGetters } from 'vuex'
 export default {
     data() {
         return {
-            courseClassList: []
+            lessonList: []
         }
     },
     computed: {
         ...mapGetters(['userinfo'])
     },
 	onLoad() {
-		this.getClassList();
+		this.getLessonList();
 	},
     methods: {
-		getClassList() {
+		getLessonList() {
 			uni.request({
 			    method: 'POST',
-			    url: `${this.doMain}/course/listCourseClass`,
+			    url: `${this.doMain}/course/parent/learnedLesson`,
 			    header: {
 			        'content-type': 'application/x-www-form-urlencoded'
 			    },
-			    data: { teacherId: this.userinfo.teacherId},
+			    data: { parentId: this.userinfo.parentId},
 			    success: res => {
 			        if (res.data.code === 0) {
-			            this.courseClassList = res.data.data;
+			            this.lessonList = res.data.data;
 			        }else{
 						uni.showToast({
 							title:res.data.fieldErrors[0].message,
@@ -63,10 +75,10 @@ page {
 	padding: 18upx 20upx 20upx;
 	background:#F2F2F2;
 }
-.clsaa-module {
+.lesson-module {
     margin-top: 30upx;
     min-height: 280upx;
-    .no-class {
+    .no-lesson {
         line-height: 280upx;
         color: #999;
         font-size: 26upx;
@@ -80,26 +92,59 @@ page {
         margin-top: 20upx;
         .top {
 			width: 100%;
-			height: 80upx;
-			line-height: 80upx;
-			font-size: 30upx;
-			color: #242039;
-			padding: 0 30upx 0 28upx;
-			text-indent: 14upx;
-			font-weight:bold;
+			.tit{
+				position: relative;
+				display: flex;
+				padding: 0 30upx 0 30upx;	
+				width: 100%;
+				height: 80upx;
+				line-height: 80upx;
+				font-size: 30upx;
+				color: #242039;
+				font-weight:bold;
+				border-bottom: 1px solid rgba(229,229,229,1);
+				.state-ttg {
+					position: absolute;
+					width: 120upx;
+					height: 36upx;
+					line-height: 36upx;
+					text-align: center;
+					font-size: 26upx;
+					color: rgba(28,92,84,1);
+					background: rgba(10, 145, 135, 1);
+					border-radius:18upx;
+					right: 10upx;
+					top: 22upx;
+				}
+			}	
+			.detail{
+				padding: 0 30upx 0 30upx;	
+				width: 100%;
+				height: 60upx;
+				line-height: 60upx;
+				font-size: 26upx;
+				.title{
+					float: left;
+					color: #999999;
+					width: 145upx;
+				}
+				.content{
+					float: left;
+					color: #333333;
+				}
+			}
         }
-        .person-nums {
+        .bottom {
+			padding: 0 30upx 0 30upx;	
             width: 100%;
             height: 80upx;
 			line-height: 80upx;
             font-size: 26upx;
-            color: rgba(102,102,102,1);
+            color: #0A9187;
             border-top: 1px solid rgba(229,229,229,1);
-            padding: 0 30upx 0 28upx;
-            text-indent: 14upx;
+            padding: 0 30upx 0 30upx;
 			font-weight:500;
         }
     }
 }
-		
 </style>
